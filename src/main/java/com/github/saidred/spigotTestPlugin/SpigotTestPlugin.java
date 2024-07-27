811c5e3bfa4cfbc9639e58a10964556e0e21654e
SaidRed <said.red018@gmail.com>
2024-07-28 07:30:39 +0900

    Initial commit

--- src/main/java/com/github/saidred/spigotTestPlugin/SpigotTestPlugin.java
+++ src/main/java/com/github/saidred/spigotTestPlugin/SpigotTestPlugin.java
@@ -1,17 +1,42 @@
 package com.github.saidred.spigotTestPlugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SpigotTestPlugin extends JavaPlugin {
+  public static JavaPlugin plugin;
+  public SpigotTestPlugin(){plugin = this;}
 
   @Override
   public void onEnable() {
-    // Plugin startup logic
-
+    // イベントサンプル
+    getServer().getPluginManager().registerEvents(new testEvent(),this);
+    // コマンドサンプル
+    getCommand("test").setExecutor(new testCommand());
   }
 
-  @Override
-  public void onDisable() {
-    // Plugin shutdown logic
+  public class testEvent implements Listener{
+    @EventHandler
+    public void onEvent(BlockPlaceEvent event){
+      getLogger().info("<< EVENT TEST LOG START >>");
+      // テストイベント
+      getLogger().info("<< EVENT TEST LOG END >>");
+    }
+  }
+
+  public static class testCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+      plugin.getLogger().info("<< COMMAND TEST LOG START >>");
+      // テストコマンド
+      plugin.getLogger().info("<< COMMAND TEST LOG END >>");
+      return true;
+    }
   }
 }
